6bdc02bcb35608c8af005b292efa4cd29696d238
Sean Knox <knoxville@gmail.com>
2013-07-02 21:41:58 -0700

    Robustify Sublime install.

--- modules/people/manifests/seanknox/applications.pp
+++ modules/people/manifests/seanknox/applications.pp
@@ -20,32 +20,48 @@ class people::seanknox::applications {
   /* include vim */
 
   include sublime_text_2
-  sublime_text_2::package { 'Package Control':
-    source => 'wbond/sublime_package_control'
-  }
-  sublime_text_2::package { 'Soda Theme':
-    source => 'buymeasoda/soda-theme'
-  }
-  sublime_text_2::package { 'Rspec':
-    source => 'SublimeText/RSpec'
-  }
-  sublime_text_2::package { 'Apply Syntax':
-    source => 'facelessuser/ApplySyntax'
-  }
-  sublime_text_2::package { 'Git':
-    source => 'kemayo/sublime-text-2-git'
-  }
-  sublime_text_2::package { 'Ctags':
-    source => 'SublimeText/CTags'
-  }
-  sublime_text_2::package { 'Zen Coding':
-    source => 'sergeche/emmet-sublime'
-  }
-  file { "/Users/${::boxen_user}/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings":
+
+  $sublime_package_dir = "/Users/${::boxen_user}/Library/Application Support/Sublime Text 2/Packages"
+
+  file {
+    [ "$sublime_package_dir/User/Preferences.sublime-settings" ]:
+    require => [ Package['SublimeText2'], Repository["/Users/${::boxen_user}/dotfiles"] ],
     target  => "/Users/${::boxen_user}/dotfiles/Preferences.sublime-settings",
-    require => Repository["/Users/${::boxen_user}/dotfiles"]
   }
 
+  file {
+    [ "$sublime_package_dir/User" ]:
+    ensure => directory,
+    require => Package['SublimeText2'],
+  }
+  sublime_text_2::package { 'Package Control':
+    source => 'wbond/sublime_package_control',
+    require => Package['SublimeText2']
+  }
+  sublime_text_2::package { 'Theme - Soda':
+    source => 'buymeasoda/soda-theme',
+    require => Package['SublimeText2'],
+  }
+  sublime_text_2::package { 'Rspec':
+    source => 'SublimeText/RSpec',
+    require => Package['SublimeText2']
+  }
+  sublime_text_2::package { 'Apply Syntax':
+    source => 'facelessuser/ApplySyntax',
+    require => Package['SublimeText2']
+  }
+  sublime_text_2::package { 'Git':
+    source => 'kemayo/sublime-text-2-git',
+    require => Package['SublimeText2']
+  }
+  sublime_text_2::package { 'Ctags':
+    source => 'SublimeText/CTags',
+    require => Package['SublimeText2']
+  }
+  sublime_text_2::package { 'Zen Coding':
+    source => 'sergeche/emmet-sublime',
+    require => Package['SublimeText2']
+  }
 
   mysql::db { 'mydb': }
 
